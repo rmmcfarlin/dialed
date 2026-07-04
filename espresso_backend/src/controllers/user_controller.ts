936e72f7b547eed162658df1306dce73ce0e8103
Ryan McFarlin <rmcfarli6048@oakton.edu>
2026-07-04 11:18:20 -0600

    started working on login route

--- espresso_backend/src/controllers/user_controller.ts
+++ espresso_backend/src/controllers/user_controller.ts
@@ -1,20 +1,9 @@
 import { Request, Response } from "express"
+import { eq } from 'drizzle-orm';
 import { type NewUser } from "../types/user_types.js"
 import { usersTable } from "../db/schema.js"
 import { db } from "../db/db.js"
 import bcrypt from "bcrypt"
-
-// GET
-export const getUser = async (req: Request, res: Response) => {
-
-    try {
-        const result = {message: "Success: /users"}
-        return res.status(201).json(result)
-    } catch {
-        return res.status(500).json({err: "unable to GET"})
-    }
-}
-
 
 // POST, create user
 export const createUser = async (req: Request, res: Response) => {
@@ -37,6 +26,26 @@ export const createUser = async (req: Request, res: Response) => {
     }
 
     return res.status(200).json({message: "User registered successfuly"})
+}
+
+// POST, login 
+export const userLogin = async (req: Request, res: Response) => {
+
+    const {email, password} = req.body
+    let userDbData
+
+    try {
+        userDbData = db
+        .select({id: usersTable.userId, firstname: usersTable.firstName, lastName: usersTable.lastName, password: usersTable.password})
+        .from(usersTable)
+        .where(eq(usersTable.email, email))
+
+    } catch (err) {
+        return res.status(404).json({error: "User email not found", err})
+    }
+
+    
+
 }
 
 // PUT
