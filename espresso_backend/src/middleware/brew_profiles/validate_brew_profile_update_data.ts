bbc6f9998a8564dfe3d9d9674810803c345faef4
Ryan McFarlin <rmcfarli6048@oakton.edu>
2026-07-26 11:18:58 -0600

    added update brew profile route / validation middleware

---
+++ espresso_backend/src/middleware/brew_profiles/validate_brew_profile_update_data.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express"
+import { targetRatioTypeEnum } from "../../db/schema.js"
+import * as z from "zod"
+
+// schema for string of digits only for IDs
+export const digitStringSchema = z.string().regex(/^\d+$/, {
+        message: "String must contain only digits",
+    });
+
+const RatioTypeEnum = z.enum(targetRatioTypeEnum.enumValues)
+
+const BrewTargetsSchema = z.object({
+    profileId: digitStringSchema,
+    targetRatioType: RatioTypeEnum.optional(), 
+    targetRatioMin: z.number().gt(0, {message: "Target Ratio min must be greater than 0"}).lte(9.9, {message: "Target ratio min must be at most 9.9"}),
+    targetRatioMax: z.number().gt(0, {message: "Target Ratio max must be greater than 0"}).lte(10, {message: "Target ratio max must at most 10"}), 
+    targetFlowMin: z.number().gt(0, {message: "Target flow min must be greater than 0 g/s"}).lte(4.9, {message: "Target flow min must at most 4.9 g/s"}),
+    targetFlowMax: z.number().gt(0, {message: "Target flow max must be greater than 0 g/s"}).lte(5, {message: "Target flow max must at most 5 g/s"})
+})
+.refine((data) => data.targetRatioMin < data.targetRatioMax, {
+    message: "Target ratio min must be less than target ratio max"
+})
+.refine((data) => data.targetFlowMin < data.targetFlowMax, {
+    message: "Target flow min must be less than target flow max"
+})
+
+export const validateBrewProfileUpdateData = (req: Request, res: Response, next: NextFunction) => {
+
+    const parsed = BrewTargetsSchema.safeParse(req.body)
+
+    if (!parsed.success) {
+        const flattened = z.flattenError(parsed.error)
+        return res.status(400).json({message: flattened})
+    } else {
+        const validProfile = parsed.data
+        req.brewProfileUpdate = validProfile
+    }
+    next()
+}
--- espresso_backend/src/routes/brew_profiles.ts
+++ espresso_backend/src/routes/brew_profiles.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { requireAuth } from '../middleware/require_auth.js'
-import { getBrewProfile, createBrewProfile, getAllBrewProfiles} from '../controllers/brew_profile_controller.js'
+import { getBrewProfile, createBrewProfile, getAllBrewProfiles, updateBrewProfile } from '../controllers/brew_profile_controller.js'
 import { validateNewBrewProfile } from '../middleware/brew_profiles/validate_new_brew_profile.js'
 import { validateNumericParam } from '../middleware/brew_profiles/validate_brew_profile_params.js'
+import { validateBrewProfileUpdateData } from '../middleware/brew_profiles/validate_brew_profile_update_data.js'
 
 const router = Router()
 
@@ -15,5 +16,7 @@ router.get('/all', requireAuth, validateNumericParam, getAllBrewProfiles)
 // POST, create new brew profile
 router.post('/new-brew-profile', requireAuth, validateNewBrewProfile, createBrewProfile)
 
+// PUT, update existing profile
+router.put('/update-profile', requireAuth, validateBrewProfileUpdateData, updateBrewProfile)
 
 export default router
